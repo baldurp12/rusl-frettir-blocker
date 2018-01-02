e65cd1bbfbccc0a8ae0e46c98d904c55709790b0
Baldur Már Pétursson <baldurp12@ru.is>
2018-01-02 18:17:45 +0000

    No changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 // Baldur Már Pétursson
 // 2.1.2018
 
-
-//
-
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return {redirectUrl: chrome.extension.getURL("blockview.html")}},
-    {urls: ["*://www.mbl.is/smartland/*", "*://*.menn.is/*", "*://*.bleikt.is/*" ]},
-    ["blocking"]);
-
+    function(details) { 
+        console.log(details.url);
+        return {redirectUrl: chrome.extension.getURL("blockview.html")}},
+    {urls: ["*://www.mbl.is/smartland/*", "*://*.menn.is/*", "*://*.bleikt.is/*", "*://*.visir.is/f/lifid/*" ]},
+    ["blocking"])
