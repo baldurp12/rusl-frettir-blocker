fa3cfbbacf7d5b395a751aeff2e80e259e92c056
Baldur Már Pétursson <baldurp12@ru.is>
2018-01-02 17:15:09 +0000

    Added menn.is and bleikt.is

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 // 2.1.2018
 
 
+//
+
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return {redirectUrl: chrome.extension.getURL("blockview.html")}; },
-    {urls: ["*://www.mbl.is/smartland/*"]},
+    function(details) { return {redirectUrl: chrome.extension.getURL("blockview.html")}},
+    {urls: ["*://www.mbl.is/smartland/*", "*://*.menn.is/*", "*://*.bleikt.is/*" ]},
     ["blocking"]);
+
