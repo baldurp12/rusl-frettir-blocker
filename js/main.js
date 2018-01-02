e0c43a11c457e03b1be1dcfd626b1f49c4ba4414
Baldur Már Pétursson <baldurp12@ru.is>
2018-01-02 16:33:27 +0000

    changed name of main js file to main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@
 // 2.1.2018
 
 
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return {redirectUrl: chrome.extension.getURL("blockview.html")}; },
+    {urls: ["*://www.mbl.is/smartland/*"]},
+    ["blocking"]);
